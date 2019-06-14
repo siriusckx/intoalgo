0be58790d75870af43320509a322150cd7502e40
siriusckx <siriusckx@gmail.com>
2019-06-15 07:55:26 +0800

    链表算法添加

---
+++ cpp/05_array/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    a = 10;
+    std::cout << "good" << std::endl;
+    return 0;
+}
---
+++ cpp/07_list/MyList.h
@@ -0,0 +1,24 @@
+#ifdef  __IA_MYLIST_H__
+#define __IA_MYLIST_H__
+
+namespace intoalgo
+{
+    //单链表反转
+    void sListReverse();
+
+    //链表中环的检测
+    void sListLoopCheck();
+
+    //两个有序的链表合并
+    void sortListMerge();
+
+    //移除链表中的的第n个结点
+    void remove();
+
+    //返回移除链表中的第n个节点
+    void reverseRemove();
+
+    void getMiddle();
+} // namespace intoalgo
+
+#endif
